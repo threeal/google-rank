440a5641dfbf34ec948663d2dc31e53b4bfe0954
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-07-17 15:49:22 +0700

    feat: add support to specify max page in `getWebsiteRank` function

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,14 +1,22 @@
 import { describe, expect, it } from "@jest/globals";
 import { getWebsiteRank, listWebsites } from "./utils";
 
-it("should list websites", async () => {
-  const websites = await listWebsites("googlethis");
-  expect(websites.length).toBeGreaterThan(0);
-  expect(websites[0]).toBe("www.npmjs.com");
+describe("list websites", () => {
+  it("should list websites", async () => {
+    const websites = await listWebsites("googlethis");
+    expect(websites.length).toBeGreaterThan(0);
+    expect(websites[0]).toBe("www.npmjs.com");
+  });
+
+  it("should list websites on specific page", async () => {
+    const websites = await listWebsites("googlethis", { page: 1 });
+    expect(websites.length).toBeGreaterThan(0);
+    expect(websites).not.toContain("www.npmjs.com");
+  });
 });
 
 describe("rank a website", () => {
-  it("should rank a found website", async () => {
+  it("should rank a website that is found", async () => {
     const rank = await getWebsiteRank("github.com", "googlethis");
     expect(rank).toBeGreaterThan(0);
   });
@@ -16,5 +24,12 @@ describe("rank a website", () => {
   it("should not rank a website that is not found", async () => {
     const rank = await getWebsiteRank("randomsite.con", "googlethis");
     expect(rank).toBe(0);
+  });
+
+  it("should rank a website that is found on specific page", async () => {
+    const rank = await getWebsiteRank("facebook.com", "googlethis", {
+      maxPage: 3,
+    });
+    expect(rank).toBeGreaterThan(0);
   });
 });
--- src/utils.ts
+++ src/utils.ts
@@ -2,11 +2,18 @@ import google from "googlethis";
 
 /**
  * Retrieves a list of websites from Google search results based on the provided keyword.
- * @param keyword The keyword to search for.
+ * @param keyword - The keyword to search for.
+ * @param opts - Additional options.
  * @returns A promise that resolves to an array of website URLs.
  */
-export async function listWebsites(keyword: string): Promise<string[]> {
-  const res = await google.search(keyword, { parse_ads: false });
+export async function listWebsites(
+  keyword: string,
+  opts?: { page?: number }
+): Promise<string[]> {
+  const res = await google.search(keyword, {
+    page: opts?.page ?? 0,
+    parse_ads: false,
+  });
   const websites: string[] = [];
   let prevWebsite = "";
   for (const result of res.results) {
@@ -19,17 +26,22 @@ export async function listWebsites(keyword: string): Promise<string[]> {
 
 /**
  * Retrieves the rank of a website in Google search results for a specific keyword.
- * @param website The website URL to check the rank for.
- * @param keyword The keyword to search for.
+ * @param website - The website URL to check the rank for.
+ * @param keyword - The keyword to search for.
+ * @param opts - Additional options.
  * @returns A promise that resolves to the rank of the website (1-based index). Returns 0 if the website is not found in the search results.
  */
 export async function getWebsiteRank(
   website: string,
-  keyword: string
+  keyword: string,
+  opts?: { maxPage?: number }
 ): Promise<number> {
-  const websites = await listWebsites(keyword);
-  for (let i = 0; i < websites.length; ++i) {
-    if (websites[i].includes(website)) return i + 1;
+  const maxPage = opts?.maxPage ?? 1;
+  for (let page = 0; page < maxPage; ++page) {
+    const websites = await listWebsites(keyword, { page });
+    for (let i = 0; i < websites.length; ++i) {
+      if (websites[i].includes(website)) return i + 1;
+    }
   }
   return 0;
 }
