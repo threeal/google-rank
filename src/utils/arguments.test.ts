cfbc9adf4113a655957f32b3f3f6ca6a668ebb5b
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-07-25 11:16:36 +0700

    feat: add `ArgumentsParser` class to parse the arguments and options of the program

--- src/utils/arguments.test.ts
+++ src/utils/arguments.test.ts
@@ -1,7 +1,6 @@
-import { beforeAll, describe, expect, it, jest } from "@jest/globals";
-import { program } from "commander";
+import { describe, expect, it, jest } from "@jest/globals";
 import { Readable } from "stream";
-import { getProgramArguments, setupProgramArguments } from "./arguments";
+import { ArgumentsParser } from "./arguments";
 
 jest.mock("fs", () => ({
   ...jest.requireActual<object>("fs"),
@@ -11,24 +10,21 @@ jest.mock("fs", () => ({
 }));
 
 describe("parse program arguments and options", () => {
-  beforeAll(() => setupProgramArguments());
-
   it("should parse arguments correctly", async () => {
+    const parser = new ArgumentsParser();
     const cmd = "node test github.com --keywords github googlethis";
-    program.parse(cmd.split(" "));
-
-    const args = await getProgramArguments();
+    const args = await parser.parse(cmd.split(" "));
     expect(args.website).toBe("github.com");
     expect(args.keywords).toStrictEqual(["github", "googlethis"]);
     expect(args.maxPage).toBe(3);
   });
 
   it("should parse arguments and options correctly", async () => {
+    const parser = new ArgumentsParser();
     const cmd =
       "node test github.com --keywords github --file keywords.txt --max-page 7";
-    program.parse(cmd.split(" "));
 
-    const args = await getProgramArguments();
+    const args = await parser.parse(cmd.split(" "));
     expect(args.website).toBe("github.com");
     expect(args.keywords).toStrictEqual([
       "github",
--- src/utils/arguments.ts
+++ src/utils/arguments.ts
@@ -1,22 +1,11 @@
-import { program } from "commander";
+import { Command } from "commander";
 import * as fs from "fs";
 import * as readline from "readline";
 
 /**
- * Sets up the arguments and options available in the program.
- */
-export function setupProgramArguments(): void {
-  program
-    .argument("<website>", "website name")
-    .option("--keywords <string...>", "keywords to search for")
-    .option("--file <string>", "file to read keywords from")
-    .option("--max-page <number>", "maximum page to search for", "3");
-}
-
-/**
  * Represents the arguments and options of the program.
  */
-export interface ProgramArguments {
+export interface Arguments {
   /** Website name. */
   website: string;
 
@@ -25,6 +14,48 @@ export interface ProgramArguments {
 
   /** Maximum page to search for. */
   maxPage: number;
+}
+
+/**
+ * Represents the arguments and options parser of the program.
+ */
+export class ArgumentsParser {
+  #program: Command;
+
+  /**
+   * Constructs a new instance of the arguments and options parser of the program.
+   */
+  constructor() {
+    this.#program = new Command();
+
+    // Sets up the arguments and options available in the program
+    this.#program
+      .argument("<website>", "website name")
+      .option("--keywords <string...>", "keywords to search for")
+      .option("--file <string>", "file to read keywords from")
+      .option("--max-page <number>", "maximum page to search for", "3");
+  }
+
+  /**
+   * Parses the arguments and options of the program.
+   * @param argv - An optional array of strings representing the command-line arguments.
+   * @returns A promise that resolves to the arguments and options.
+   */
+  async parse(argv?: readonly string[]): Promise<Arguments> {
+    this.#program.parse(argv);
+    const opts = this.#program.opts();
+
+    let keywords = opts.keywords;
+    if (opts.file !== undefined) {
+      keywords = keywords.concat(await readKeywordsFromFile(opts.file));
+    }
+
+    return {
+      website: this.#program.args[0],
+      maxPage: parseInt(opts.maxPage, 10),
+      keywords,
+    };
+  }
 }
 
 /**
@@ -42,23 +73,4 @@ async function readKeywordsFromFile(filename: string): Promise<string[]> {
     if (trimmedLine.length > 0) keywords.push(trimmedLine);
   }
   return keywords;
-}
-
-/**
- * Gets the arguments and options of the program.
- * @returns A promise that resolves to the arguments and options.
- */
-export async function getProgramArguments(): Promise<ProgramArguments> {
-  const opts = program.opts();
-
-  let keywords = opts.keywords;
-  if (opts.file !== undefined) {
-    keywords = keywords.concat(await readKeywordsFromFile(opts.file));
-  }
-
-  return {
-    website: program.args[0],
-    maxPage: parseInt(opts.maxPage, 10),
-    keywords,
-  };
 }
