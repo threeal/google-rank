5e06dec3b24d22183557534e99c76c80520603dc
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-07-25 11:31:58 +0700

    test: expand `parse program arguments and options` test

--- src/utils/arguments.test.ts
+++ src/utils/arguments.test.ts
@@ -10,28 +10,54 @@ jest.mock("fs", () => ({
 }));
 
 describe("parse program arguments and options", () => {
-  it("should parse arguments correctly", async () => {
+  it("should parse nothing", async () => {
     const parser = new ArgumentsParser();
-    const cmd = "node test github.com --keywords github googlethis";
+    const cmd = "node test github.com";
     const args = await parser.parse(cmd.split(" "));
-    expect(args.website).toBe("github.com");
-    expect(args.keywords).toStrictEqual(["github", "googlethis"]);
-    expect(args.maxPage).toBe(3);
+    expect(args).toStrictEqual({
+      website: "github.com",
+      keywords: [],
+      maxPage: 3,
+    });
+  });
+
+  it("should parse keywords correctly", async () => {
+    const parser = new ArgumentsParser();
+    const cmd = "node test github.com --keywords github gihtub";
+    const args = await parser.parse(cmd.split(" "));
+    expect(args).toStrictEqual({
+      website: "github.com",
+      keywords: ["github", "gihtub"],
+      maxPage: 3,
+    });
+  });
+
+  it("should parse file correctly", async () => {
+    const parser = new ArgumentsParser();
+    const cmd = "node test github.com --file keywords.txt";
+    const args = await parser.parse(cmd.split(" "));
+    expect(args).toStrictEqual({
+      website: "github.com",
+      keywords: ["googlethis", "googlethat", "googlethis github"],
+      maxPage: 3,
+    });
   });
 
   it("should parse arguments and options correctly", async () => {
     const parser = new ArgumentsParser();
     const cmd =
-      "node test github.com --keywords github --file keywords.txt --max-page 7";
-
+      "node test github.com --keywords github gihtub --file keywords.txt --max-page 7";
     const args = await parser.parse(cmd.split(" "));
-    expect(args.website).toBe("github.com");
-    expect(args.keywords).toStrictEqual([
-      "github",
-      "googlethis",
-      "googlethat",
-      "googlethis github",
-    ]);
-    expect(args.maxPage).toBe(7);
+    expect(args).toStrictEqual({
+      website: "github.com",
+      keywords: [
+        "github",
+        "gihtub",
+        "googlethis",
+        "googlethat",
+        "googlethis github",
+      ],
+      maxPage: 7,
+    });
   });
 });
