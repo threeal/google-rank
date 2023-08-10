d060462618ef8f6e657313cf81dee660b8977aeb
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-08-10 17:16:25 +0700

    test: use esmock to mock imported modules

--- test/arguments.test.mjs
+++ test/arguments.test.mjs
@@ -1,16 +1,24 @@
+import esmock from "esmock";
 import { expect } from "expect";
-// import { Readable } from "stream";
-import { ArgumentsParser } from "../dist/internal/arguments.mjs";
+import path from "path";
+import { Readable } from "stream";
+import { createRequire } from "module";
 
-// jest.mock("fs", () => ({
-//   ...jest.requireActual<object>("fs"),
-//   createReadStream: () => {
-//     return Readable.from(["googlethis\n\n\ngooglethat\n", "googlethis github"]);
-//   },
-// }));
+const require = createRequire(import.meta.url);
 
 describe("parse program arguments and options", () => {
+  const mockImport = () =>
+    esmock("../dist/internal/arguments.mjs", {
+      [require.resolve("fs").replaceAll(path.sep, "/")]: {
+        createReadStream: () => {
+          return Readable.from(["googlethis\n\n\ngooglethat\n", "googlethis github"]);
+        },
+      },
+    });
+
   it("should parse nothing", async () => {
+    const { ArgumentsParser } = await mockImport();
+
     const parser = new ArgumentsParser();
     const cmd = "node test github.com";
     const args = await parser.parse(cmd.split(" "));
@@ -22,6 +30,8 @@ describe("parse program arguments and options", () => {
   });
 
   it("should parse keywords correctly", async () => {
+    const { ArgumentsParser } = await mockImport();
+
     const parser = new ArgumentsParser();
     const cmd = "node test github.com --keywords github gihtub";
     const args = await parser.parse(cmd.split(" "));
@@ -33,6 +43,8 @@ describe("parse program arguments and options", () => {
   });
 
   it("should parse file correctly", async () => {
+    const { ArgumentsParser } = await mockImport();
+
     const parser = new ArgumentsParser();
     const cmd = "node test github.com --file keywords.txt";
     const args = await parser.parse(cmd.split(" "));
@@ -44,6 +56,8 @@ describe("parse program arguments and options", () => {
   });
 
   it("should parse arguments and options correctly", async () => {
+    const { ArgumentsParser } = await mockImport();
+
     const parser = new ArgumentsParser();
     const cmd =
       "node test github.com --keywords github gihtub --file keywords.txt --max-page 7";
