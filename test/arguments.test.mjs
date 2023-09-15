61d52324b24817074eb0f1f020e6e9b5f3021d5f
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-09-15 17:15:33 +0700

    test: revert back to use Jest for testing

--- test/arguments.test.mjs
+++ test/arguments.test.mjs
@@ -1,27 +1,20 @@
-import esmock from "esmock";
-import path from "path";
+import { expect, jest } from "@jest/globals";
 import { Readable } from "stream";
-import { createRequire } from "module";
 
-const require = createRequire(import.meta.url);
+jest.unstable_mockModule("fs", () => ({
+  __esModule: true,
+  createReadStream: () =>
+    Readable.from(["googlethis\n\n\ngooglethat\n", "googlethis github"]),
+}));
 
 describe("parse program arguments and options", () => {
-  const mockImport = () =>
-    esmock("../dist/internal/arguments.mjs", {
-      [require.resolve("fs").replaceAll(path.sep, "/")]: {
-        createReadStream: () =>
-          Readable.from(["googlethis\n\n\ngooglethat\n", "googlethis github"]),
-      },
-    });
-
   it("should parse nothing", async () => {
-    const { ArgumentsParser } = await mockImport();
-
+    const { ArgumentsParser } = await import("../dist/internal/arguments.mjs");
     const parser = new ArgumentsParser();
     const cmd = "node test github.com";
 
     const prom = parser.parse(cmd.split(" "));
-    return prom.should.eventually.to.be.deep.equal({
+    return expect(prom).resolves.toStrictEqual({
       website: "github.com",
       keywords: ["github.com"],
       maxPage: 3,
@@ -29,13 +22,12 @@ describe("parse program arguments and options", () => {
   });
 
   it("should parse keywords correctly", async () => {
-    const { ArgumentsParser } = await mockImport();
-
+    const { ArgumentsParser } = await import("../dist/internal/arguments.mjs");
     const parser = new ArgumentsParser();
     const cmd = "node test github.com --keywords github gihtub";
 
     const prom = parser.parse(cmd.split(" "));
-    return prom.should.eventually.to.be.deep.equal({
+    return expect(prom).resolves.toStrictEqual({
       website: "github.com",
       keywords: ["github", "gihtub"],
       maxPage: 3,
@@ -43,13 +35,12 @@ describe("parse program arguments and options", () => {
   });
 
   it("should parse file correctly", async () => {
-    const { ArgumentsParser } = await mockImport();
-
+    const { ArgumentsParser } = await import("../dist/internal/arguments.mjs");
     const parser = new ArgumentsParser();
     const cmd = "node test github.com --file keywords.txt";
 
     const prom = parser.parse(cmd.split(" "));
-    return prom.should.eventually.to.be.deep.equal({
+    return expect(prom).resolves.toStrictEqual({
       website: "github.com",
       keywords: ["googlethis", "googlethat", "googlethis github"],
       maxPage: 3,
@@ -57,14 +48,13 @@ describe("parse program arguments and options", () => {
   });
 
   it("should parse arguments and options correctly", async () => {
-    const { ArgumentsParser } = await mockImport();
-
+    const { ArgumentsParser } = await import("../dist/internal/arguments.mjs");
     const parser = new ArgumentsParser();
     const cmd =
       "node test github.com --keywords github gihtub --file keywords.txt --max-page 7";
 
     const prom = parser.parse(cmd.split(" "));
-    return prom.should.eventually.to.be.deep.equal({
+    return expect(prom).resolves.toStrictEqual({
       website: "github.com",
       keywords: [
         "github",
--- test/format.test.mjs
+++ test/format.test.mjs
@@ -1,28 +1,31 @@
+import { expect } from "@jest/globals";
 import chalk from "chalk";
 import { formatKeywordRank } from "../dist/internal/format.mjs";
 
 describe("format the rank of a keyword as a string", () => {
   it("should format undefined rank correctly", () => {
+    const str = formatKeywordRank("a keyword", undefined);
     const mark = chalk.blackBright("?");
-    formatKeywordRank("a keyword", undefined).should.be.equal(
-      `page ${mark}  rank ${mark}  a keyword`,
-    );
+    expect(str).toBe(`page ${mark}  rank ${mark}  a keyword`);
   });
 
   it("should format green rank correctly", () => {
-    formatKeywordRank("a keyword", { page: 0, rank: 2 }).should.be.equal(
+    const str = formatKeywordRank("a keyword", { page: 0, rank: 2 });
+    expect(str).toBe(
       `page ${chalk.greenBright(1)}  rank ${chalk.greenBright(3)}  a keyword`,
     );
   });
 
   it("should format green page and red rank correctly", () => {
-    formatKeywordRank("a keyword", { page: 0, rank: 3 }).should.be.equal(
+    const str = formatKeywordRank("a keyword", { page: 0, rank: 3 });
+    expect(str).toBe(
       `page ${chalk.greenBright(1)}  rank ${chalk.redBright(4)}  a keyword`,
     );
   });
 
   it("should format red rank correctly", () => {
-    formatKeywordRank("a keyword", { page: 1, rank: 0 }).should.be.equal(
+    const str = formatKeywordRank("a keyword", { page: 1, rank: 0 });
+    expect(str).toBe(
       `page ${chalk.redBright(2)}  rank ${chalk.redBright(1)}  a keyword`,
     );
   });
