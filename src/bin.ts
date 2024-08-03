099884683959fd08319e09fddee0b5a720517c6c
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-03 22:05:10 +0700

    refactor: remove `run` function in `bin.mts`

--- src/bin.ts
+++ src/bin.ts
@@ -8,30 +8,24 @@ import { getWebsiteRank, WebsiteRank } from "./rank.js";
 
 type RankPromise = Promise<WebsiteRank | undefined>;
 
-async function run() {
-  const parser = new ArgumentsParser();
-  const args = await parser.parse();
+const parser = new ArgumentsParser();
+const args = await parser.parse();
 
-  const rankByKeywords: [string, RankPromise][] = [];
-  for (const keyword of args.keywords) {
-    const prom = getWebsiteRank(args.website, keyword, {
-      maxPage: args.maxPage,
-    });
-    rankByKeywords.push([keyword, prom]);
-  }
-
-  process.stdout.write(
-    `Ranks for ${chalk.blueBright(args.website)} website:\n`,
-  );
-
-  const loading = ora("Getting ranks...");
-  loading.start();
-  for (const [keyword, prom] of rankByKeywords) {
-    loading.text = `Getting ranks of ${chalk.blueBright(keyword)} keyword...`;
-    const str = formatKeywordRank(keyword, await prom);
-    process.stdout.write(`\r\x1b[K${str}\n`);
-  }
-  loading.stop();
+const rankByKeywords: [string, RankPromise][] = [];
+for (const keyword of args.keywords) {
+  const prom = getWebsiteRank(args.website, keyword, {
+    maxPage: args.maxPage,
+  });
+  rankByKeywords.push([keyword, prom]);
 }
 
-run();
+process.stdout.write(`Ranks for ${chalk.blueBright(args.website)} website:\n`);
+
+const loading = ora("Getting ranks...");
+loading.start();
+for (const [keyword, prom] of rankByKeywords) {
+  loading.text = `Getting ranks of ${chalk.blueBright(keyword)} keyword...`;
+  const str = formatKeywordRank(keyword, await prom);
+  process.stdout.write(`\r\x1b[K${str}\n`);
+}
+loading.stop();
